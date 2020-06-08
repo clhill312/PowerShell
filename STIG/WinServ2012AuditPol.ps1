cc74b9fc7437950bc0b715580f44e5bfebe3e905
Carl Hill <clhill312@users.noreply.github.com>
2020-06-08 14:05:38 +0200

    Create WinServ2012AuditPol.ps1

---
+++ STIG/WinServ2012AuditPol.ps1
@@ -0,0 +1,31 @@
+auditpol /set /subcategory:"Central Staging Policy" /success:enable
+auditpol /set /subcategory:"Central Staging Policy" /failure:enable
+auditpol /set /subcategory:"Credential Validation" /success:enable
+auditpol /set /subcategory:"Credential Validation" /failure:enable
+auditpol /set /subcategory:"Other Account Management Events" /success:enable
+auditpol /set /subcategory:"Security Group Management" /success:enable
+auditpol /set /subcategory:"User Account Management" /success:enable
+auditpol /set /subcategory:"User Account Management" /failure:enable
+auditpol /set /subcategory:"Process Creation" /success:enable
+auditpol /set /subcategory:"Logoff" /success:enable
+auditpol /set /subcategory:"Logon" /success:enable
+auditpol /set /subcategory:"Logon" /failure:enable
+auditpol /set /subcategory:"Special Logon" /success:enable
+auditpol /set /subcategory:"Audit Policy Change" /success:enable
+auditpol /set /subcategory:"Audit Policy Change" /failure:enable
+auditpol /set /subcategory:"Authorization Policy Change" /success:enable
+auditpol /set /subcategory:"Sensitive Privilege Use" /success:enable
+auditpol /set /subcategory:"Sensitive Privilege Use" /failure:enable
+auditpol /set /subcategory:"IPsec Driver" /success:enable
+auditpol /set /subcategory:"IPsec Driver" /failure:enable
+auditpol /set /subcategory:"Security State Change" /success:enable
+auditpol /set /subcategory:"Security System Extension" /success:enable
+auditpol /set /subcategory:"System Integrity" /success:enable
+auditpol /set /subcategory:"System Integrity" /failure:enable
+auditpol /set /subcategory:"Removable Storage" /success:enable
+auditpol /set /subcategory:"Removable Storage" /failure:enable
+auditpol /set /subcategory:"Authorization Policy Change" /success:enable
+auditpol /set /subcategory:"Account Lockout" /success:enable
+auditpol /set /subcategory:"Account Lockout" /failure:enable
+auditpol /set /subcategory:"Other System Events" /success:enable
+auditpol /set /subcategory:"Other System Events" /failure:enable
